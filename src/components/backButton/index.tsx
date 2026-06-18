836c5f5c2de37be747b4d8c84758d6bee3ac394c
jukalbf <lgbr212@gmail.com>
2026-06-18 15:31:36 -0300

    feat: Lógica de back button

--- src/components/backButton/index.tsx
+++ src/components/backButton/index.tsx
@@ -1,18 +1,37 @@
 "use client";
 
 import styles from "./styles.module.scss";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
+import { DEFAULT_PARAMS } from "@/constants/defaultParams";
 
 const BackButton = ({ style }: { style?: React.CSSProperties }) => {
+  const listPages = [
+    "fornecedores",
+    "gastos",
+    "usuarios",
+    "pedidos",
+    "clientes",
+  ] as const;
   const router = useRouter();
+  const pathname = usePathname();
+  const handleBack = () => {
+    for (const page in listPages) {
+      if (pathname.includes(listPages[page])) {
+        router.push(`/${listPages[page]}?${DEFAULT_PARAMS}`);
+        return;
+      }
+    }
+
+    router.back();
+  };
 
   return (
     <button
       className={styles.backButton}
       style={style}
       type={"button"}
-      onClick={() => router.back()}
+      onClick={handleBack}
     >
       Voltar
     </button>
