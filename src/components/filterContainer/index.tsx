1aff0a78c3afa7322753b0b0212b70d278242ef1
jukalbf <lgbr212@gmail.com>
2026-06-18 15:40:32 -0300

    feat: Lógica de searchParams no filtro

--- src/components/filterContainer/index.tsx
+++ src/components/filterContainer/index.tsx
@@ -1,11 +1,52 @@
+"use client";
+
 import styles from "./styles.module.scss";
 import { ReactNode } from "react";
+import { BsSearch } from "react-icons/bs";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-const FilterContainer = ({ children }: { children: ReactNode }) => {
+type FilterContainerProps = {
+  children: ReactNode;
+  setEndpoint?: (value: string) => void;
+  target?: string;
+};
+
+const FilterContainer = (props: FilterContainerProps) => {
+  const { children, setEndpoint, target } = props;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = () => {
+    if (setEndpoint && target)
+      setEndpoint(`${target}/filter?${searchParams.toString()}`);
+  };
+  const clearFilters = () => {
+    const defaultParams = searchParams
+      .toString()
+      .split("&")
+      .filter(
+        (filter) => filter.includes("page") || filter.includes("per_page"),
+      )
+      .join("&");
+
+    if (setEndpoint && target) setEndpoint(`${target}?${defaultParams}`);
+
+    router.push(`${pathname}?${defaultParams}`);
+  };
+
   return (
     <div className={styles.filterContainer}>
       <h5 className={styles.filterTitle}>Filtros</h5>
-      <div className={styles.filters}>{children}</div>
+      <div className={styles.filters}>
+        {children}
+        <button className={styles.searchButton} onClick={handleSearch}>
+          <BsSearch />
+        </button>
+        <button className={styles.clearFilters} onClick={() => clearFilters()}>
+          <span>Limpar filtros</span>
+        </button>
+      </div>
     </div>
   );
 };
