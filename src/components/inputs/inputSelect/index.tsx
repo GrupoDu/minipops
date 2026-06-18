412bc5ab1ca2cae5b74b7eef2e53d391f41f058a
jukalbf <lgbr212@gmail.com>
2026-06-18 16:08:09 -0300

    feat: Tornando param dinâmico

--- src/components/inputs/inputSelect/index.tsx
+++ src/components/inputs/inputSelect/index.tsx
@@ -1,23 +1,39 @@
 import styles from "./styles.module.scss";
 import { ChangeEvent, CSSProperties } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 type InputSelectProps = {
   label: string;
   options: { value: string; label: string }[];
-  value: string;
-  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
   placeholder?: string;
   style?: CSSProperties;
+  filterTarget?: string;
 };
 
 const InputSelect = (props: InputSelectProps) => {
-  const { options, value, label, onChange, placeholder, style } = props;
+  const { options, value, label, onChange, placeholder, style, filterTarget } =
+    props;
   const defaultValue = placeholder || "Selecione um opção";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (onChange) onChange(e);
+
+    if (filterTarget) {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(filterTarget, e.target.value);
+      router.push(`${pathname}?${params.toString()}`);
+    }
+  };
 
   return (
     <label style={style} className={"inputContainer"}>
       <span className={styles.label}>{label}</span>
-      <select className={"input"} value={value} onChange={onChange}>
+      <select className={"input"} value={value} onChange={handleChange}>
         <option value="">{defaultValue}</option>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
