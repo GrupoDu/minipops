467c3cc2da14b65a025e5848170a370092b5310c
jukalbf <lgbr212@gmail.com>
2026-06-18 16:25:11 -0300

    refactor(pedidos): Implementando filtros
    - Movendo filtros da página principal para o componente
    - Implementando funcionalidade de filtros

--- src/app/(pages)/pedidos/page.tsx
+++ src/app/(pages)/pedidos/page.tsx
@@ -1,20 +1,7 @@
 import PageHeader from "@/components/pageHeader";
-import TableHeader from "@/components/tableHeader";
-import FilterContainer from "@/components/filterContainer";
-import InputText from "@/components/inputs/inputText";
-import { InputDate } from "@/components/inputs/inputDate";
 import OrderList from "@/components/orderList";
 
 function OrdersPage() {
-  const tableHeaderTitles = [
-    "Pedido",
-    "Cliente",
-    "Obra",
-    "Status",
-    "Total",
-    "Ações",
-  ];
-
   return (
     <div className={"pageContainer"}>
       <PageHeader
@@ -25,16 +12,6 @@ function OrdersPage() {
         addButton={"Adicionar Pedido"}
       />
       <div className={"mainContent"}>
-        <FilterContainer>
-          <InputText
-            type={"text"}
-            label={"Cliente"}
-            filterTarget={"client"}
-            isSearch={true}
-            placeholder={"Pesquisar cliente"}
-          />
-          <InputDate label={"Prazo"} />
-        </FilterContainer>
         <OrderList />
       </div>
     </div>
--- src/components/orderList/index.tsx
+++ src/components/orderList/index.tsx
@@ -1,55 +1,121 @@
 "use client";
 
 import styles from "./styles.module.scss";
-import { Order } from "@/types/order.interface";
-import { usePathname } from "next/navigation";
+import { OrderPagination } from "@/types/order.interface";
+import { usePathname, useSearchParams } from "next/navigation";
 import { dateFormatter } from "@/utils/dateFormatter";
 import { priceFormatter } from "@/utils/priceFormatter";
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 import Link from "next/link";
 import { CgDetailsMore } from "react-icons/cg";
 import ListTemplate from "@/components/listTemplate";
 import { EmptyList } from "@/components/emptyList";
 import useFetch from "@/hooks/useFetch";
+import FilterContainer from "@/components/filterContainer";
+import { InputDate } from "@/components/inputs/inputDate";
+import { Client } from "@/types/client.interface";
+import InputSelect from "@/components/inputs/inputSelect";
+import { STATUS_CONSTANT } from "@/constants/status.constant";
 
 function OrderList() {
-  const { data: orders, isLoading } = useFetch<Order[]>("orders");
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page");
+  const per_page = searchParams.get("per_page");
+  const client = searchParams.get("client");
+  const date = searchParams.get("date");
+  const [clientFilter, setClientFilter] = useState("");
+  const [statusFilter, setStatusFilter] = useState("");
+  const [endpoint, setEndpoint] = useState(
+    `orders?page=${page ? page : 1}&per_page=${per_page ? per_page : 10}${client ? `&client=${client}` : ""}${date ? `&created_at=${date}` : ""}`,
+  );
+  const { data, isLoading } = useFetch<OrderPagination>(endpoint);
+  const { data: clients } = useFetch<Client[]>("clients");
   const pathname = usePathname();
   const isDashboard = pathname.includes("dashboard");
-  const pendingOrders = orders?.filter(
+  const pendingOrders = data?.orders.filter(
     (order) => order.order_status === "Pendente",
   );
-  const ordersToUse = isDashboard ? pendingOrders : orders;
+  const ordersToUse = isDashboard ? pendingOrders : data?.orders;
   const isOrdersEmpty = !ordersToUse || ordersToUse.length < 1;
-  const heads = ["ID", "Cliente", "Obra", "Status", "Total", "Ações"];
+  const tableHeads = {
+    id: "ID",
+    client: "Cliente",
+    building: "Obra",
+    status: "Status",
+    total: "Total",
+    actions: "Ações",
+  } as const;
+  const clientsOptions =
+    clients?.map((client) => ({
+      value: client.client_uuid,
+      label: client.client_name,
+    })) || [];
+  const statusOptions = Object.values(STATUS_CONSTANT).map((status) => ({
+    value: status,
+    label: status,
+  }));
 
   if (isLoading)
     return <div className={styles.fetchLoading}>Carregando...</div>;
-  if (isOrdersEmpty) return <EmptyList targetName={"pedido"} />;
 
   return (
-    <ListTemplate heads={heads}>
-      {ordersToUse.map((order) => (
-        <tr key={order.custom_order_id}>
-          <td className={styles.orderIdContainer}>
-            <span className={styles.orderId}>{order.custom_order_id}</span>
-            <span className={styles.orderDate}>
-              {dateFormatter(order.created_at)}
-            </span>
-          </td>
-          <td>{order.clients.client_name}</td>
-          <td>{order.delivery.building}</td>
-          <td style={statusStyle(order.order_status)}>{order.order_status}</td>
-          <td>{priceFormatter(order.total_price)}</td>
-          <td className={styles.buttonContainer}>
-            <Link href={`/pedidos/${order.custom_order_id}`}>
-              <CgDetailsMore color={"#000000"} />
-              <span className={styles.buttonText}>Detalhes</span>
-            </Link>
-          </td>
-        </tr>
-      ))}
-    </ListTemplate>
+    <>
+      <FilterContainer setEndpoint={setEndpoint} target={"orders"}>
+        <InputSelect
+          label={"Cliente"}
+          options={clientsOptions}
+          value={clientFilter}
+          filterTarget={"client"}
+          onChange={(e) => setClientFilter(e.target.value)}
+        />
+        <InputSelect
+          label={"Status"}
+          options={statusOptions}
+          filterTarget={"status"}
+          value={statusFilter}
+          onChange={(e) => setStatusFilter(e.target.value)}
+        />
+        <InputDate label={"Data de emissão"} param={"created_at"} />
+      </FilterContainer>
+      {isOrdersEmpty ? (
+        <EmptyList targetName={"pedido"} />
+      ) : (
+        <ListTemplate heads={Object.values(tableHeads)}>
+          {ordersToUse.map((order) => (
+            <tr key={order.custom_order_id}>
+              <td>
+                <div className={styles.orderIdContainer}>
+                  <span className={styles.orderId}>
+                    {order.custom_order_id}
+                  </span>
+                  <span className={styles.orderDate}>
+                    {dateFormatter(order.created_at)}
+                  </span>
+                </div>
+              </td>
+              <td>{order.clients.client_name}</td>
+              <td>{order.delivery.building}</td>
+              <td>
+                <div style={statusStyle(order.order_status)}>
+                  {order.order_status}
+                </div>
+              </td>
+
+              <td>{priceFormatter(order.total_price)}</td>
+              <td>
+                <Link
+                  className={styles.buttonContainer}
+                  href={`/pedidos/${order.custom_order_id}`}
+                >
+                  <CgDetailsMore color={"#000000"} />
+                  <span className={styles.buttonText}>Detalhes</span>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </ListTemplate>
+      )}
+    </>
   );
 }
 
