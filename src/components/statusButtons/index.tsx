73c7299c811e877114b8c9c039c06949bbc1716b
jukalbf <lgbr212@gmail.com>
2026-06-18 15:59:35 -0300

    feat: Componente para alterar status do pedido

---
+++ src/components/statusButtons/index.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import styles from "./styles.module.scss";
+import { TbCancel } from "react-icons/tb";
+import { toast } from "react-toastify";
+import { api } from "@/services/api";
+import { BiCheck } from "react-icons/bi";
+import BackButton from "@/components/backButton";
+import { Modal } from "@/components/modal";
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { DEFAULT_PARAMS } from "@/constants/defaultParams.constant";
+
+export const StatusButtons = ({
+  slug,
+  show,
+}: {
+  slug: string;
+  show: boolean;
+}) => {
+  const [showModal, setShowModal] = useState(false);
+  const [status, setStatus] = useState("");
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const changeStatus = async () => {
+    enum acceptableStatus {
+      DONE = "Concluído",
+      CANCELED = "Cancelado",
+    }
+
+    const isValidStatus = Object.values(acceptableStatus).includes(
+      status as acceptableStatus,
+    );
+
+    if (!isValidStatus) {
+      toast.error("Status inválido.");
+      return;
+    }
+
+    try {
+      const response = await api.put(`/orders/${slug}`, {
+        order_status: status,
+      });
+
+      const data = await response.data;
+
+      toast.success(data.message);
+      router.push(`${pathname}?${DEFAULT_PARAMS}`);
+    } catch (err) {
+      const error = err as Error;
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        type={"warning"}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        onClickConfirm={() => changeStatus()}
+      />
+      <div className={styles.statusBtnsContainer}>
+        <div className={styles.backButton}>
+          <BackButton />
+        </div>
+        <div
+          className={styles.statusBtns}
+          style={{ display: show ? "flex" : "none" }}
+        >
+          <button
+            type={"button"}
+            className={styles.doneButton}
+            onClick={() => {
+              setStatus("Concluído");
+              setShowModal(true);
+            }}
+          >
+            <span>Marcar como Finalizado</span>
+            <BiCheck />
+          </button>
+          <button
+            type={"button"}
+            onClick={() => {
+              setStatus("Cancelado");
+              setShowModal(true);
+            }}
+            className={styles.cancelButton}
+          >
+            <span>Marcar como Cancelado</span>
+            <TbCancel />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
