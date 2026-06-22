95f615ea6dcea9599ac915cd9d96e9393ad5d2db
jukalbf <lgbr212@gmail.com>
2026-06-22 16:45:23 -0300

    feat: Condicional de dashboard para pedido
    - Condicional de link para registrar novo pedido

--- src/components/emptyList/index.tsx
+++ src/components/emptyList/index.tsx
@@ -1,12 +1,21 @@
 import styles from "./styles.module.scss";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export const EmptyList = ({ targetName }: { targetName: string }) => {
+  const pathname = usePathname();
+  const isDashboard = pathname === "/dashboard";
+
   return (
     <div className={styles.emptyList}>
       <span>Nenhum registro encontrado.</span>
       <span>
         Adicione um novo registro clicando em{" "}
-        <strong>&quot;+ Adicionar {targetName}&quot;</strong>
+        {isDashboard ? (
+          <Link href={`/pedidos/add`}>Registrar Pedido</Link>
+        ) : (
+          <strong>&quot;+ Adicionar {targetName}&quot;</strong>
+        )}
       </span>
     </div>
   );
