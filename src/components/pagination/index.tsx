ff5ffe21884cb27941965689d33a3c84d82ec3d3
jukalbf <lgbr212@gmail.com>
2026-09-02 07:41:20 -0300

    feat: Pulo da primeira e ultima página
    Adicionando lógica para mostrar primeira e última página

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -2,6 +2,7 @@
 
 import styles from "./styles.module.scss";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { setQueryParams } from "@/utils/setQueryParams";
 
 type PaginationProps = {
   maxPage: number;
@@ -16,7 +17,9 @@ export const Pagination = (props: PaginationProps) => {
 
   return (
     <div className={styles.pagination}>
+      <DisplayFirstPage sequenceFirstIndex={currentSequence[0]} />
       <DisplayPages currentSequence={currentSequence} />
+      <DisplayLastPage maxPage={maxPage} />
     </div>
   );
 };
@@ -26,30 +29,94 @@ function DisplayPages({ currentSequence }: { currentSequence: number[] }) {
   const pathname = usePathname();
   const router = useRouter();
 
-  const handleChangeParams = (page: number) => {
-    const params = new URLSearchParams(searchParams.toString());
-    params.set("page", page.toString());
-    router.push(`${pathname}?${params.toString()}`);
-  };
-
   const isSelected = (page: number) =>
     page === Number(searchParams.get("page"));
 
+  return currentSequence.map((page) => (
+    <button
+      key={page}
+      onClick={() => {
+        const params = setQueryParams({
+          searchParams,
+          key: "page",
+          value: page.toString(),
+        });
+        router.push(`${pathname}?${params}`);
+      }}
+      className={`${styles.paginationItem} ${isSelected(page) ? styles.isSelected : ""}`}
+    >
+      {page}
+    </button>
+  ));
+}
+
+function DisplayFirstPage({
+  sequenceFirstIndex,
+}: {
+  sequenceFirstIndex: number;
+}) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const isNotFirstSequence = sequenceFirstIndex > 4;
+
+  if (!isNotFirstSequence) return;
+
   return (
     <>
-      {currentSequence.map((page) => (
-        <button
-          key={page}
-          onClick={() => handleChangeParams(page)}
-          className={`${styles.paginationItem} ${isSelected(page) ? styles.isSelected : ""}`}
-        >
-          {page}
-        </button>
-      ))}
+      <button
+        onClick={() => {
+          const params = setQueryParams({
+            searchParams,
+            key: "page",
+            value: "1",
+          });
+          router.push(`${pathname}?${params}`);
+        }}
+        className={styles.paginationItem}
+      >
+        1
+      </button>
+      <span className={styles.spaceDots}>...</span>
     </>
   );
 }
 
+function DisplayLastPage({ maxPage }: { maxPage: number }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const page = Number(searchParams.get("page")) || 1;
+  const isLastSequence = page + 4 >= maxPage;
+
+  if (isLastSequence) return;
+
+  return (
+    <>
+      <span className={styles.spaceDots}>...</span>
+      <button
+        onClick={() => {
+          const params = setQueryParams({
+            searchParams,
+            key: "page",
+            value: maxPage.toString(),
+          });
+          router.push(`${pathname}?${params}`);
+        }}
+        className={styles.paginationItem}
+      >
+        {maxPage}
+      </button>
+    </>
+  );
+}
+
+/**
+ * Retorna a sequência de números para exibição na paginação.
+ *
+ * @param page - Página atual
+ * @param allPages - Array com todas as páginas
+ */
 function getCurrentSequence(page: number, allPages: number[]) {
   const isFirstPage = page === 1;
   const sequenceEnd = page + 4;
